21e07817d34ac8b897c875e07195f315432121ee
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-02 10:27:46 -0500

    first commit

---
+++ api_request.py
@@ -0,0 +1,11 @@
+import requests
+from environ import API_KEY # My API KEY from my environ module
+from pprint import pprint
+
+city = input("Enter a city: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=" + city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data["timezone"])
