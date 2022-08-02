036310d2725ff12ef9759575a5dc17a64643c650
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-02 11:30:09 -0500

    Learning tkinter

--- test_interface.py
+++ test_interface.py
@@ -1,4 +1,5 @@
 
+from cgitb import text
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -18,6 +19,8 @@ ttk.Label(frm, text="API Search").grid(column=1, row=0)
 ttk.Button(frm, text="Search", command=root.destroy).grid(column=0, row=1)
 # ttk.Label(frm, text="Colum 1").grid(column=1, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=1)
+
+ttk.Combobox(frm, text="Combobox").grid(column=3, row=1)
 
 # ttk.Label(frm,text="Style test",foreground="black", background="white").grid(column=3, row=0) # Explicit Style
 
