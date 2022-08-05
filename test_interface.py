88b75dfb172fe6e36157f231862630f7f6748a0f
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-05 12:04:24 -0500

    Cleaning test_interface.py

--- test_interface.py
+++ test_interface.py
@@ -1,11 +1,9 @@
-
-from cgitb import text
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
 
 root = Tk()
-frm = ttk.Frame(root, padding=100)
+root.geometry("800x600")
+frm = ttk.Frame(root, padding=200)
 frm.grid()
 
 # Style
@@ -13,16 +11,14 @@ style = ttk.Style()
 style.configure("BW.TLabel", foreground="black", background="white")
 
 # Elements
-# ttk.Label(frm,text="Style test", style="BW.TLabel").grid(column=2, row=0) # Using Style
-# Elements without style
-ttk.Label(frm, text="API Search").grid(column=1, row=0)
-ttk.Button(frm, text="Search", command=root.destroy).grid(column=0, row=1)
-# ttk.Label(frm, text="Colum 1").grid(column=1, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=1)
 
-ttk.Combobox(frm, text="Combobox").grid(column=3, row=1)
-
-# ttk.Label(frm,text="Style test",foreground="black", background="white").grid(column=3, row=0) # Explicit Style
+# Entry
+ttk.Label(frm, text='Search your country:').grid(column=1, row=0)
+nombre=StringVar()
+nomb=ttk.Entry(frm, textvariable=nombre).grid(column=1, row=1)
+# Buttons
+ttk.Button(frm, text="Search", command=root.quit).grid(column=0, row=2)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=2)
 
 root.title("API Call") # Title
 root.mainloop()
